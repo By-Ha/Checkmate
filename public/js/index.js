4031241360cfd748dd4429e9ead835d1c6a23f01
By_Ha <2051261534@qq.com>
2020-05-06 13:54:11 +0800

    KaTeX

--- public/js/index.js
+++ public/js/index.js
@@ -125,6 +125,11 @@ $(() => {
                 }
             }
         })
+        renderMathInElement(document.body,{
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false}]
+        });
     }
     rebuild();
     function getSourcePost() {
