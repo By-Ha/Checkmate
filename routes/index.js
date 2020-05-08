9bbfe731ca1905a68e03de5f62758dd99bce2699
By_Ha <2051261534@qq.com>
2020-05-08 13:20:31 +0800

    增加500状态

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database/database');
 var ejs = require('ejs');
+var createError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     if (req.session.username) {
         db.queryTypeContent(0, 1, 10, function (err, dat) {
-            if (err) res.render('index', { title: '首页', username: req.session.username, uid: req.session.uid, dat: null });
-            res.render('index', { title: '首页', username: req.session.username, uid: req.session.uid, dat: dat, page: 1 });
+            if (err) next(createError(500));
+            else res.render('index', { title: '首页', username: req.session.username, uid: req.session.uid, dat: dat, page: 1 });
         });
     } else {
         res.redirect('/login');
@@ -17,7 +18,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/page/:pid', function (req, res, next) {
     db.queryTypeContent(0, req.params.pid, 10, function (err, dat) {
-        if (err) res.send(null);
+        if (err) next(createError(500));
         else {
             if(dat.length == 10)
                 res.render('index', { title: '首页', username: req.session.username, uid: req.session.uid, dat: dat, page: req.params.pid });
