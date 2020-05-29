b2198b21411a9111f0def9e4ebc4375802d35580
By_Ha <2051261534@qq.com>
2020-05-29 15:15:44 +0800

    修复BUg

--- routes/api.js
+++ routes/api.js
@@ -110,11 +110,25 @@ router.post('/post', function (req, res) {
     if (req.body.content == undefined || req.body.content.length <= 2 || req.body.content.length >= 1000) { res.json({ status: ('error'), msg: '内容长度不符合规范' }); return; }
     if (req.body.content.match(/[\u0600-\u06FF]/) != null) { res.json({ status: ('error'), msg: '包含不允许的阿拉伯字符' }); return; }
     if (req.body.type != 0) { res.json({ status: ('error'), msg: '类型错误' }); return; }
-    db.post(req.session.username, req.body.type, req.body.content, function (err, dat) {
+    db.getUserInfo(req.session.uid, (err, dat) => {
         if (err) { res.json({ status: ('error'), msg: '数据库错误' }); return; }
-        else { res.json({ status: ('success'), msg: '发送成功' }); return; }
+        if (dat.shuoshuoban == true) { res.json({ status: ('error'), msg: '您的说说权限已被封禁' }); return; }
+        db.post(req.session.username, req.body.type, req.body.content, function (err, dat) {
+            if (err) { res.json({ status: ('error'), msg: '数据库错误' }); return; }
+            else { res.json({ status: ('success'), msg: '发送成功' }); return; }
+        })
     })
 });
+
+router.post('/updatepost', function (req, res) {
+    if (req.session.username == undefined) { res.json({ status: ('error'), msg: '请先登录' }); return; }
+    if (req.body.content == undefined || req.body.content.length <= 2 || req.body.content.length >= 1000) { res.json({ status: ('error'), msg: '内容长度不符合规范' }); return; }
+    if (req.body.content.match(/[\u0600-\u06FF]/) != null) { res.json({ status: ('error'), msg: '包含不允许的阿拉伯字符' }); return; }
+    db.updatePost(req.body.pid, req.session.username, req.body.content, function (err, dat) {
+        if (err) { res.json({ status: ('error'), msg: err }); return; }
+        else { res.json({ status: ('success'), msg: '修改成功' }); return; }
+    })
+})
 
 // comment
 router.get('/comment', function (req, res) {
@@ -160,15 +174,6 @@ router.post('/getSourcePost', function (req, res) {
     db.getSourcePost(req.body.pid, (err, dat) => {
         if (err) { res.json({ 'status': 'error', 'msg': 'No Such User' }); return; }
         else { res.json({ 'status': 'success', 'msg': dat[0].content }); return; }
-    })
-})
-
-router.post('/updatepost', function (req, res) {
-    if (req.session.username == undefined) { res.json({ status: ('error'), msg: '请先登录' }); return; }
-    if (req.body.content == undefined || req.body.content.length <= 2 || req.body.content.length >= 1000) { res.json({ status: ('error'), msg: '内容长度不符合规范' }); return; }
-    db.updatePost(req.body.pid, req.session.username, req.body.content, function (err, dat) {
-        if (err) { res.json({ status: ('error'), msg: err }); return; }
-        else { res.json({ status: ('success'), msg: '修改成功' }); return; }
     })
 })
 
